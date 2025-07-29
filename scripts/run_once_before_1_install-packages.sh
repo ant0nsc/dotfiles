0682cfa17d3b79befb87874753ee359c8901a44f
Anton Schwaighofer <antonsc@microsoft.com>
2025-07-29 15:48:28 +0100

    fix gcm installation

--- scripts/run_once_before_1_install-packages.sh
+++ scripts/run_once_before_1_install-packages.sh
@@ -9,14 +9,11 @@ fi
 
 # Install and configure Git Credential Manager
 # This is necessary for authentication to Azure DevOps
-GCM_FILE="gcm-linux_amd64.2.6.1.deb"
-if [ ! -f /tmp/$GCM_FILE ]; then
-    echo "Downloading Git Credential Manager..."
-    wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.6.1/$GCM_FILE /tmp/$GCM_FILE
-else
-    echo "Git Credential Manager already downloaded."
-fi
-sudo dpkg -i /tmp/$GCM_FILE
+GCM_LOCAL=$(mktemp /tmp/gcm-linux_XXXXXX.deb)
+GCM_REMOTE="v2.6.1/gcm-linux_amd64.2.6.1.deb"
+echo "Downloading Git Credential Manager..."
+wget -O $GCM_LOCAL https://github.com/git-ecosystem/git-credential-manager/releases/download/$GCM_REMOTE
+sudo dpkg -i $GCM_LOCAL
 
 # Configure the GCM to not try to create popups, they eventually break on WSL
 git-credential-manager configure --no-ui
