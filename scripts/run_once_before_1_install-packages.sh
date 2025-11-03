508aed60db90a3d32e85048bed1b9ef819f687d3
Anton Schwaighofer <antonsc@microsoft.com>
2025-11-03 16:28:16 +0000

    template for zdiff

--- scripts/run_once_before_1_install-packages.sh
+++ scripts/run_once_before_1_install-packages.sh
@@ -5,15 +5,23 @@ sudo usermod -s `which zsh` $USER
 # On WSL, extra libraries are needed for the Git Credential Manager
 if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
     sudo apt-get install -y libsm6 libice6
+else
+    # Install and configure Git Credential Manager
+    # This is necessary for authentication to Azure DevOps
+    GCM_LOCAL=$(mktemp /tmp/gcm-linux_XXXXXX.deb)
+    GCM_REMOTE="v2.6.1/gcm-linux_amd64.2.6.1.deb"
+    echo "Downloading Git Credential Manager..."
+    wget -O $GCM_LOCAL https://github.com/git-ecosystem/git-credential-manager/releases/download/$GCM_REMOTE
+    sudo dpkg -i $GCM_LOCAL
+
+    # Configure the GCM to not try to create popups, they eventually break on WSL
+    git-credential-manager configure --no-ui
 fi
 
-# Install and configure Git Credential Manager
-# This is necessary for authentication to Azure DevOps
-GCM_LOCAL=$(mktemp /tmp/gcm-linux_XXXXXX.deb)
-GCM_REMOTE="v2.6.1/gcm-linux_amd64.2.6.1.deb"
-echo "Downloading Git Credential Manager..."
-wget -O $GCM_LOCAL https://github.com/git-ecosystem/git-credential-manager/releases/download/$GCM_REMOTE
-sudo dpkg -i $GCM_LOCAL
-
-# Configure the GCM to not try to create popups, they eventually break on WSL
-git-credential-manager configure --no-ui
+if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+    curl -sSL -O https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    rm packages-microsoft-prod.deb
+    sudo apt-get update
+    sudo apt-get install azcopy
+fi
